74d87d5621a9f0c674ba71e71928105af47aaee1
Mamun-100 <rashid202412@gmail.com>
2021-01-08 17:30:51 +0600

    update upto 08-01-2021

---
+++ src/app/models/product.ts
@@ -0,0 +1,18 @@
+// import { threadId } from "worker_threads";
+
+export class Product {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+
+    constructor(id, name, description ='', price = 0, imageUrl = 'https://images-na.ssl-images-amazon.com/images/I/71wPwmxo2NL._SL1500_.jpg'){
+        this.id = id
+        this.name = name
+        this.description = description
+        this.price = price
+        this.imageUrl = imageUrl
+
+    }
+}
